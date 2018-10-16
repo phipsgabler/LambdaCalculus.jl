80e6765ebdb1cd4884ead2dcdc7585af718abad0
Philipp Gabler <phipsgabler@users.noreply.github.com>
2018-10-16 10:15:57 +0200

    Fix conversions and test them

--- src/conversions.jl
+++ src/conversions.jl
@@ -11,19 +11,22 @@ equivalent_type(::Type{Named.Abs}) = DeBruijn.Abs
 
 
 convert(::Type{<:DeBruijn.Term}, t::Named.Term) = convert(DeBruijn.Term, t, collect(freevars(t)))
-convert(::Type{DeBruijn.Term}, t::NT, ctx::Vector{Symbol}) where {NT<:Named.Term} =
-    convert(equivalent_type(NT), t, ctx)
-convert(::Type{DeBruijn.Var}, v::Named.Var, ctx::Vector{Symbol}) =
-    DeBruijn.Var(findfirst(isequal(v.name), ctx))
-convert(::Type{DeBruijn.App}, t::Named.App, ctx::Vector{Symbol}) =
-    DeBruijn.App(convert(DeBruijn.Term, t.car, ctx), convert(DeBruijn.Term, t.cdr, ctx))
-convert(::Type{DeBruijn.Abs}, t::Named.Abs, ctx::Vector{Symbol}) =
-    DeBruijn.Abs(convert(DeBruijn.Term, t.body, [t.boundname; ctx]))
+convert(::Type{DeBruijn.Term}, t::NT, fv::Vector{Symbol}) where {NT<:Named.Term} =
+    convert(equivalent_type(NT), t, fv)
+convert(::Type{DeBruijn.Var}, v::Named.Var, fv::Vector{Symbol}) =
+    DeBruijn.Var(findlast(isequal(v.name), fv))
+convert(::Type{DeBruijn.App}, t::Named.App, fv::Vector{Symbol}) =
+    DeBruijn.App(convert(DeBruijn.Term, t.car, fv), convert(DeBruijn.Term, t.cdr, fv))
+convert(::Type{DeBruijn.Abs}, t::Named.Abs, fv::Vector{Symbol}) =
+    DeBruijn.Abs(convert(DeBruijn.Term, t.body, [t.boundname; fv]))
 
-convert(::Type{Named.Term}, t::DT, ctx::Vector{Symbol}) where {DT<:DeBruijn.Term} =
-    convert(equivalent_type(DT), t, ctx)
-convert(::Type{Named.Var}, t::DeBruijn.Var, ctx::Vector{Symbol}) = Named.Var(ctx[t.index])
-convert(::Type{Named.App}, t::DeBruijn.App, ctx::Vector{Symbol}) =
-    Named.App(convert(Named.Term, t.car, ctx), convert(Named.Term, t.cdr, ctx))
-convert(::Type{Named.Abs}, t::DeBruijn.Abs, ctx::Vector{Symbol}) =
-    Named.Abs(ctx[1], convert(Named.Term, t.body, ctx[2:end]))
+
+convert(::Type{Named.Term}, t::DT, fv::Vector{Symbol}) where {DT<:DeBruijn.Term} =
+    convert(equivalent_type(DT), t, fv)
+convert(::Type{Named.Var}, t::DeBruijn.Var, fv::Vector{Symbol}) = Named.Var(fv[t.index])
+convert(::Type{Named.App}, t::DeBruijn.App, fv::Vector{Symbol}) =
+    Named.App(convert(Named.Term, t.car, fv), convert(Named.Term, t.cdr, fv))
+convert(::Type{Named.Abs}, t::DeBruijn.Abs, fv::Vector{Symbol}) =
+    let boundname = freshname(:x, fv)
+        Named.Abs(boundname, convert(Named.Term, t.body, [boundname; fv]))
+    end
--- test/runtests.jl
+++ test/runtests.jl
@@ -29,30 +29,6 @@ Log.global_logger(Log.SimpleLogger(stderr, Log.Debug))
         @test freevars(D.@λ x -> y) == Set([2])
         @test (D.@λ x) ≃ (D.@lambda y)
     end
-    
-    # testcode = @rawquote begin
-    #     id = @λ x -> x
-    #     l = @λ $id(a)
-    #     r = @λ (y -> y)(a)
-    #     @test l ≃ r
-
-        
-    #     # @test alpha_equivalent(id_xx, id_x_result)
-    #     # @test alpha_equivalent(id_xn, id_x_result)
-    #     # @test alpha_equivalent(id_nn, id_n_result)
-    #     # @test alpha_equivalent(id_nx, id_n_result)
-
-    #     # t = @evaluate x -> (x -> x)(y)
-    #     # t_result = @lambda x -> y
-    # end
-    
-    # for ns in [:(Lambdas.Named), :(Lambdas.DeBruijn)]
-    #     code = quote
-    #         using $ns
-    #         $testcode
-    #     end
-
-    # end
 end
 
 @testset "Conversion" begin
@@ -70,17 +46,15 @@ end
     
     for t in named_lambdas
         @test convert(N.Term, convert(D.Term, t), varnames) ≃ t
-        # @test convert(N.Term, convert(D.Term, t), [:x, :y, :z]) == t
     end
 
     for t in debruijn_lambdas
-        @test convert(D.Term, convert(N.Term, t, varnames)) ≃ t
-        # @test convert(D.Term, convert(N.Term, t, [:x, :y, :z])) == t
+        @test convert(D.Term, convert(N.Term, t, [:x, :y, :z])) == t
     end
 
-    # for (tn, tx) in zip(named_lambdas, debruijn_lambdas)
-    #     @test convert(D.Term, tn) ≃ tx
-    #     @test convert(N.Term, tx, varnames) ≃ tn
-    # end
+    for (tn, tx) in zip(named_lambdas, debruijn_lambdas)
+        @test convert(D.Term, tn) ≃ tx
+        @test convert(N.Term, tx, varnames) ≃ tn
+    end
 end
 
