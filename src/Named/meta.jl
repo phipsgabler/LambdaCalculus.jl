be182336f8041f935e77aaff52e19b3015110838
Philipp Gabler <phipsgabler@users.noreply.github.com>
2018-10-26 10:14:02 +0200

    Rename macro implementation methods for Named

--- src/Named/meta.jl
+++ src/Named/meta.jl
@@ -1,34 +1,34 @@
 export @lambda, @λ
 
-function macroconvert(expr::Expr)
+function meta_convert(expr::Expr)
     if expr.head == :call
         # TODO: handle :* case
-        mapfoldl(e -> macroconvert(e), (e, arg) -> :(App($e, $arg)), expr.args)
+        mapfoldl(e -> meta_convert(e), (e, arg) -> :(App($e, $arg)), expr.args)
     elseif expr.head == :->
         boundname = Meta.quot(expr.args[1])
-        body = macroconvert(expr.args[2])
+        body = meta_convert(expr.args[2])
         :(Abs($boundname, $body))
     elseif expr.head == :$ && length(expr.args) == 1
         esc(expr.args[1])
     elseif expr.head == :block
         # Such trivial blocks are used by the parser in lambdas to add metadata.
         # They consist of a LineNumberNode followed by the actual expression.
-        macroconvert(expr.args[end])
+        meta_convert(expr.args[end])
     else
         error("unhandled syntax: $expr")
     end
 end
 
-macroconvert(name::Symbol) = :(Var($(Meta.quot(name))))
-macroconvert(other) = error("unhandled literal: $other")
+meta_convert(name::Symbol) = :(Var($(Meta.quot(name))))
+meta_convert(other) = error("unhandled literal: $other")
 
 
 "Convert a (well-formed) Julia expression to a `Term`."
 macro lambda(expr)
-    macroconvert(expr)
+    meta_convert(expr)
 end
 
 "Convert a (well-formed) Julia expression to a `Term`."
 macro λ(expr)
-    macroconvert(expr)
+    meta_convert(expr)
 end
