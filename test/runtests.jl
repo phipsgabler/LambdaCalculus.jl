3a3679e26abefcfb51bbca514a628f849932f907
Philipp Gabler <phipsgabler@users.noreply.github.com>
2018-11-02 11:20:31 +0100

    Update tests (failing now)

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,8 +21,10 @@ Log.global_logger(Log.SimpleLogger(stderr, Log.Debug))
         @test freevars(N.@λ x -> y) == Set([:y])
         @test (N.@λ x) ≄ (N.@lambda y)
 
+        # interpolation of named terms is _not_ hygienic
         t = N.@lambda x -> y
-        @test_broken (N.@lambda y -> $t) ≃ (N.@lambda z1 -> z2 -> y)
+        @test (N.@lambda y -> $t) ≄ (N.@lambda z1 -> z2 -> y)
+        @test (N.@lambda y -> $t) ≃ (N.@lambda z1 -> z2 -> z1)
     end
 
     @testset "DeBruijn" begin
@@ -34,6 +36,7 @@ Log.global_logger(Log.SimpleLogger(stderr, Log.Debug))
         @test freevars(D.@λ x -> y) == Set([1])
         @test_broken (D.@λ x) ≄ (D.@lambda y)
 
+        # interpolation of de Bruijn terms _is_ hygienic (inserted terms are shifted)
         t = D.@lambda x -> y
         @test (D.@lambda y -> $t) ≃ (D.@lambda z1 -> z2 -> y)
     end
@@ -45,15 +48,16 @@ Log.global_logger(Log.SimpleLogger(stderr, Log.Debug))
         @test l ≃ r
 
         @test freevars(LN.@λ x -> y) == Set([:y])
-        # @test_broken (LN.@λ x) ≄ (LN.@lambda y)
+        @test (LN.@λ x) ≄ (LN.@lambda y)
 
+        # interpolation of locally nameless terms _is_ hygienic (inserted terms are shifted)
         t = LN.@lambda x -> y
         @test (LN.@lambda y -> $t) ≃ (LN.@lambda z1 -> z2 -> y)
     end
 end
 
 @testset "Conversion" begin
-    varnames = NamingContext([:x, :y, :z])
+    Γ = NamingContext([:x, :y, :z])
     
     named_lambdas = [(N.@lambda x -> x),
                      (N.@lambda (x -> x(x))(x -> x(x))),
@@ -68,16 +72,16 @@ end
     @test convert(D.Term, N.@lambda((x -> (x -> x)))) ≃ D.@lambda x -> (y -> y)
     
     for t in named_lambdas
-        @test_skip convert(N.Term, convert(D.Term, t), varnames) ≃ t
+        @test_skip convert(N.Term, convert(D.Term, t, Γ), Γ) ≃ t
     end
 
     for t in debruijn_lambdas
-        @test convert(D.Term, convert(N.Term, t, varnames)) == t
+        @test convert(D.Term, convert(N.Term, t, Γ), Γ) == t
     end
 
     for (tn, tx) in zip(named_lambdas, debruijn_lambdas)
-        @test convert(D.Term, tn) == tx
-        @test_skip convert(N.Term, tx, varnames) ≃ tn
+        @test convert(D.Term, tn, Γ) == tx
+        @test_skip convert(N.Term, tx, Γ) ≃ tn 
     end
 end
 
@@ -87,12 +91,14 @@ end
                  D.@lambda((x -> x)(z -> (x -> x)(z))),
                  D.@lambda(y -> (f -> x -> f(x))(y)),
                  D.@lambda((f -> x -> f(x))(f -> x -> f(x))),
-                 D.@lambda((f -> x -> f(f(x)))(f -> x -> f(f(x))))]
+                 D.@lambda((f -> x -> f(f(x)))(f -> x -> f(f(x)))),
+                 D.@lambda((x -> x)(y))]
         results = [D.@lambda(x -> x),
                    D.@lambda(z -> z),
                    D.@lambda(f -> x -> f(x)),
                    D.@lambda(f -> x -> f(x)),
-                   D.@lambda(x -> y -> x(x(x(x(y)))))]
+                   D.@lambda(x -> y -> x(x(x(x(y))))),
+                   D.@lambda(y)]
         
         for (t, r) in zip(terms, results)
             @test evaluate(t, 100) ≃ r
@@ -104,12 +110,14 @@ end
                  LN.@lambda((x -> x)(z -> (x -> x)(z))),
                  LN.@lambda(y -> (f -> x -> f(x))(y)),
                  LN.@lambda((f -> x -> f(x))(f -> x -> f(x))),
-                 LN.@lambda((f -> x -> f(f(x)))(f -> x -> f(f(x))))]
+                 LN.@lambda((f -> x -> f(f(x)))(f -> x -> f(f(x)))),
+                 LN.@lambda((x -> x)(y))]
         results = [LN.@lambda(x -> x),
                    LN.@lambda(z -> z),
                    LN.@lambda(f -> x -> f(x)),
                    LN.@lambda(f -> x -> f(x)),
-                   LN.@lambda(x -> y -> x(x(x(x(y)))))]
+                   LN.@lambda(x -> y -> x(x(x(x(y))))),
+                   LN.@lambda(y)]
         
         for (t, r) in zip(terms, results)
             @test evaluate(t, 100) ≃ r
